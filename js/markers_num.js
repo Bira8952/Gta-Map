e24e196e6a009cac9eb833e96a1ec47de19b92b9
Bira8952 <onurcan.bozkurt6262@hotmail.com>
2023-07-26 00:43:18 +0200

    Neu

---
+++ js/markers_num.js
@@ -0,0 +1,12 @@
+L.marker([-3811.5, 2530.5], {icon: cardIcon9}).addTo(map)
+  .bindPopup("<h2>Ampullen Farmer</h3><p><a href='https://i.imgur.com/WSAo1Ig.png' data-lightbox='image-1' data-title='Route 68 1'>Open In-Game Screenshot</a><div class='input-group input-group-sm mb-3'></div></div></p>");
+L.marker([-4391.5, 4764], {icon: cardIcon9}).addTo(map)
+  .bindPopup("<h2>Ampullen Verarbeiter 1</h3><p><a href='https://i.imgur.com/WSAo1Ig.png' data-lightbox='image-1' data-title='Route 68 1'>Open In-Game Screenshot</a><div class='input-group input-group-sm mb-3'></div></div></p>");
+L.marker([-3832.25, 5293.75], {icon: cardIcon9}).addTo(map)
+  .bindPopup("<h2>Ampullen Verarbeiter 2</h3><p><a href='https://i.imgur.com/WSAo1Ig.png' data-lightbox='image-1' data-title='Route 68 1'>Open In-Game Screenshot</a><div class='input-group input-group-sm mb-3'></div></div></p>");
+L.marker([-5997, 5579.5], {icon: cardIcon4}).addTo(map)
+  .bindPopup("<h2>--------</h3><p><a href='https://i.imgur.com/WSAo1Ig.png' data-lightbox='image-1' data-title='Route 68 1'>Open In-Game Screenshot</a><div class='input-group input-group-sm mb-3'></div></div></p>");
+L.marker([-6603, 4905], {icon: cardIcon4}).addTo(map)
+  .bindPopup("<h2>--------</h3><p><a href='https://i.imgur.com/WSAo1Ig.png' data-lightbox='image-1' data-title='Route 68 1'>Open In-Game Screenshot</a><div class='input-group input-group-sm mb-3'></div></div></p>");
+L.marker([-6598.25, 3022], {icon: cardIcon2}).addTo(map)
+  .bindPopup("<h2>--------</h3><p><a href='https://i.imgur.com/WSAo1Ig.png' data-lightbox='image-1' data-title='Route 68 1'>Open In-Game Screenshot</a><div class='input-group input-group-sm mb-3'></div></div></p>");
